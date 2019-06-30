d5bea8c72097981c4aa4414cbdeeb824175a59c1
Megan Fago <meganlaynefago@gmail.com>
2019-06-30 23:24:42 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React, {Component} from 'react'
+
+class App extends Component {
+  
+  
+  
+  render(){
+    return (
+      <div>
+      </div>
+      
+      )
+  }
+  
+}
+
+export default App;
